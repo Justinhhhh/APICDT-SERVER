4e7ac7a2b950e31db4acea953a32e29f38f91b30
AmosChong <amoschong0820@gmail.com>
2022-12-08 17:35:21 +0800

    Modify header component

--- client/components/header.js
+++ client/components/header.js
@@ -1,21 +1,12 @@
-import { Ma_Shan_Zheng } from '@next/font/google'
 import styles from '../styles/header.module.css'
-import logo from '../public/logo.png'
 import Image from 'next/image'
 
-const MaShanZheng = Ma_Shan_Zheng({
-    weight: '400',
-    subsets: ['latin'],
-    size: '500'
-})
-
-function Header() {
-    console.log(MaShanZheng.className)
+function Header({ image, font, title }) {
     return (
-        <div className={`${MaShanZheng.className} ${styles.container}`}>
+        <div className={`${font.className} ${styles.container}`}>
             <div className={styles.header}>
-                <Image src={logo} width={200}></Image>
-                <div>第十一届亚太辩论比赛</div>
+                <Image src={image} width={200}></Image>
+                <div>{title}</div>
             </div>
         </div>
     );
