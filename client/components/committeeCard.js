c490deec7b28047bf51409194572ec7a4d3c9a91
AmosChong <amoschong0820@gmail.com>
2022-12-09 20:27:31 +0800

    Created committeeCard component

---
+++ client/components/committeeCard.js
@@ -0,0 +1,24 @@
+import { Card, CardHeader, CardBody, CardFooter, Stack, Heading, Text, Divider, ButtonGroup, Button } from '@chakra-ui/react'
+import Image from 'next/image'
+import individualPic from '../public/IndividualPic.jpeg'
+
+function CommitteeCard() {
+    return ( 
+        <Card maxW='sm' ml={100} pb={20} mb={20} mt={20}>
+        <CardBody>
+            <Image
+            src={individualPic}
+            borderRadius='lg'
+            />
+            <Stack mt='6' spacing='3'>
+            <Heading size='md'>Amos Chong</Heading>
+            <Text>
+                The most outstanding student in NTU. Webmaster of APICDT 2023.
+            </Text>
+            </Stack>
+        </CardBody>
+        </Card>
+     );
+}
+
+export default CommitteeCard;
