776168dcafc7055d797687b268f7b6e016f15996
Han Zhiguang <145914897justin@gmail.com>
2022-12-11 15:38:05 +0800

    update footer and timlimes

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -1,32 +1,78 @@
-import { ButtonGroup, Container, IconButton, Stack, Text } from '@chakra-ui/react'
+import {
+  Button,
+  ButtonGroup,
+  Container,
+  Divider,
+  IconButton,
+  Input,
+  Stack,
+  Text,
+} from '@chakra-ui/react'
 import * as React from 'react'
-import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa'
+import { FaFacebook, FaGithub, FaInstagram, FaLinkedin, FaTwitter, FaWeibo } from 'react-icons/fa'
 
-function Footer(){
-    return(
-  <Container as="footer" role="contentinfo" py={{ base: '12', md: '16' }}>
-    <Stack spacing={{ base: '4', md: '5' }}>
-      <Stack justify="space-between" direction="row" align="center">
-        <ButtonGroup variant="ghost">
-          <IconButton
-            as="a"
-            href="#"
-            aria-label="LinkedIn"
-            icon={<FaLinkedin fontSize="1.25rem" />}
-          />
-          <IconButton as="a" href="#" aria-label="GitHub" icon={<FaGithub fontSize="1.25rem" />} />
-          <IconButton
-            as="a"
-            href="https://www.instagram.com/apchinesedebate/" target="_blank" 
-            aria-label="Twitter"
-            icon={<FaTwitter fontSize="1.25rem" />}
-          />
-        </ButtonGroup>
+
+export default function Footer() {
+  return (
+    <Container as="footer" role="contentinfo">
+    <Stack
+      spacing="8"
+      direction={{ base: 'column', md: 'row' }}
+      justify="space-between"
+      py={{ base: '12', md: '16' }}
+    >
+      <Stack
+        direction={{ base: 'column-reverse', md: 'column', lg: 'row' }}
+        spacing={{ base: '12', md: '8' }}
+      >
+        <Stack direction="column" spacing="40">
+          <Stack spacing="0" minW="39" flex="1">
+            <Text fontSize="sm" fontWeight="semibold" color="subtle">
+            <Text mb={30} fontSize={36} fontFamily={"Ma Shan Zheng"}>联系方式</Text>
+            </Text>
+            <Stack spacing="2" shouldWrapChildren>
+            <Text fontSize={20} fontFamily={"Ma Shan Zheng"}>何佳萱：+65 83148709</Text>
+            <Text fontSize={20} fontFamily={"Ma Shan Zheng"}>何智圆：+65 86500369</Text>
+            <Text fontSize={20} fontFamily={"Ma Shan Zheng"}>沈佳欣：+65 91087660</Text>
+            </Stack>
+          </Stack>
+          <Stack spacing="0" minW="39" flex="1">
+            <Text fontSize="sm" fontWeight="semibold" color="subtle">
+            <Text mb={30} fontSize={36} fontFamily={"Ma Shan Zheng"}>合作伙伴 Title Partner</Text>
+            </Text>
+            <Stack spacing="4" shouldWrapChildren>
+            <Text mb={30} fontSize={20} fontFamily={"Ma Shan Zheng"}>何佳萱：+65 83148709</Text>
+          </Stack>
+          </Stack>
+        </Stack>
       </Stack>
+    </Stack>
+    <Divider />
+    <Stack
+      pt="6"
+      pb="10"
+      justify="space-between"
+      direction={{ base: 'column-reverse', md: 'row' }}
+      align="center"
+    >
       <Text fontSize="sm" color="subtle">
-        &copy; {new Date().getFullYear()} Nanyang Technological University, Inc. All rights reserved.
+      &copy; {new Date().getFullYear()} Nanyang Technological University, Inc. All rights reserved.
       </Text>
+      <ButtonGroup variant="ghost">
+        <IconButton
+          as="a"
+          href="#"
+          aria-label="LinkedIn"
+          icon={<FaLinkedin fontSize="1.25rem" />}
+        />
+        <IconButton as="a" href="#" aria-label="GitHub" icon={<FaGithub fontSize="1.25rem" />} />
+        <IconButton as="a" href="#" aria-label="Twitter" icon={<FaTwitter fontSize="1.25rem" />} />
+        <IconButton as="a" href="#" aria-label="Facebook" icon={<FaFacebook fontSize="1.25rem" />} />
+        <IconButton as="a" href="#" aria-label="Facebook" icon={<FaWeibo fontSize="1.25rem" />} />
+        <IconButton as="a" href="#" aria-label="Facebook" icon={<FaInstagram fontSize="1.25rem" />} />
+      </ButtonGroup>
+      
     </Stack>
   </Container>
-)}
-export default Footer;
+  )
+}
