433919e588c36ac6b78273a55cea966f34503959
AmosChong <amoschong0820@gmail.com>
2022-12-15 14:50:35 +0800

    Created server folder

--- client/pages/about.js
+++ client/pages/about.js
@@ -1,11 +1,14 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Text, Heading } from "@chakra-ui/react";
 import Committees from "../components/committees";
 import "@fontsource/ma-shan-zheng"
 
 function About() {
     return (
         <Box fontFamily={"Ma Shan Zheng"}>
-            <Text fontSize={60} pl={20} pt={20}>筹委团</Text>
+            <Heading pl={20} pt={20} fontFamily={"Ma Shan Zheng"} fontSize={72}>关于我们</Heading>
+            <Text pl={20} pt={10} fontSize={60}>历史</Text>
+            <Text pl={20} pt={5} fontSize={36}>亚太辩于1981年举办第一届。目前已连续举办31年</Text>
+            <Text pl={20} pt={20} fontSize={60}>筹委团</Text>
             <Committees />
         </Box>
     );
