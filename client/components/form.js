6a1b79a8aa35c29d5e2b2b098d65dd483ca5c49f
Han Zhiguang <145914897justin@gmail.com>
2022-12-17 11:43:14 +0800

    combine dialog alert and button tgt

--- client/components/form.js
+++ client/components/form.js
@@ -4,14 +4,13 @@ import {
     Input,
     Select,
     Flex,
-    Button,
     Box,
     Heading
 } from '@chakra-ui/react'
 import country from '../public/country.json'
 import "@fontsource/inder";
 import "@fontsource/ma-shan-zheng"
-
+import Button from './Button';
 
 function Form({ information }) {
     const { Countries } = country
@@ -46,7 +45,7 @@ function Form({ information }) {
                     </Flex>
                 </Flex>
             </FormControl>
-            <Button type='submit' mt={'29'} h={'40px'} w={'120px'} colorScheme={'purple'}><Box fontWeight={'600px'}>Submit</Box> </Button>
+            <Button text='Submit' description="请检查清楚才点击“确认”，一旦提交就无法再做修改，也无法重新提交。" title="Discard Changes?" falsebutton="取消" checkbutton="确定" > <Box fontWeight={'600px'}></Box></Button>
         </Box>
     );
 }
--- client/pages/_app.js
+++ client/pages/_app.js
@@ -2,12 +2,10 @@ import { ChakraProvider } from '@chakra-ui/react'
 import '../styles/globals.css'
 import NavBar from '../components/NavBar'
 import Footer from '../components/Footer'
-import CheckDialog from '../components/checkDialog'
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
       <NavBar/>
-      <CheckDialog/>
       <Component {...pageProps} />
     <Footer/>
     </ChakraProvider>
