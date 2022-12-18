53be4c5cb16ed45ded2315292b6216990293ef38
AmosChong <amoschong0820@gmail.com>
2022-12-18 23:12:19 +0800

    Added live page

---
+++ client/pages/live.js
@@ -0,0 +1,12 @@
+import { Box, Heading } from '@chakra-ui/react'
+import '@fontsource/ma-shan-zheng'
+
+function Live() {
+    return ( 
+        <Box ml={10} mt={10} h={'92vh'}>
+            <Heading fontFamily={'Ma Shan Zheng'}>直播</Heading>
+        </Box>
+     );
+}
+
+export default Live;
