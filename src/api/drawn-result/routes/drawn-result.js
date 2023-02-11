8b5c413a1314b91536d1556121426ff2daf1fc01
Han Zhiguang <145914897justin@gmail.com>
2023-02-12 00:50:33 +0800

    first commit

---
+++ src/api/drawn-result/routes/drawn-result.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * drawn-result router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::drawn-result.drawn-result');
---
+++ src/api/drawn-result/services/drawn-result.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * drawn-result service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::drawn-result.drawn-result');
