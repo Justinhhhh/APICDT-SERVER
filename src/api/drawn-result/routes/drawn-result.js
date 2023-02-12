13d96c3b7976fdd6e387ae68165cbada25227b1c
Han Zhiguang <93465028+Justinhhhh@users.noreply.github.com>
2023-02-12 23:34:14 +0800

    Delete src directory

--- src/api/drawn-result/routes/drawn-result.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * drawn-result router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::drawn-result.drawn-result');
