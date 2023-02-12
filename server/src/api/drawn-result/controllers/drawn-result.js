78bdef9004736e4670c9b9b8ac5b215f629122e2
Han Zhiguang <93465028+Justinhhhh@users.noreply.github.com>
2023-02-12 23:34:33 +0800

    Delete server/src directory

--- server/src/api/drawn-result/controllers/drawn-result.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * drawn-result controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::drawn-result.drawn-result');
--- server/src/api/drawn-result/services/drawn-result.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * drawn-result service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::drawn-result.drawn-result');
